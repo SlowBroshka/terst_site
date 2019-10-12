875ec6c9a7c6476b2e3c21fad0074965b082fe6c
Vladislav <vlad.kuz12@gmail.com>
2019-10-12 20:25:13 +0300

    Add login page tests

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,6 +6,7 @@ class BasePage():
     def __init__(self, browser, url, search_t_sec=10):
         self.browser = browser
         self.url = url
+        self.search_t_sec = search_t_sec
         self.browser.implicitly_wait(search_t_sec)
 
     def open(self):
@@ -17,3 +18,9 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+
+    def has_str_in_url(self, str):
+        return str in self.browser.current_url
+
+    def get_curr_url(self):
+        return self.browser.current_url
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .login_page import LoginPage
 from .locators import MainPageLocators as Mpl
 from selenium.webdriver.common.by import By
 
@@ -8,6 +9,8 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*Mpl.LOGIN_LINK)
         login_link.click()
+        return LoginPage(browser=self.browser, url=self.url,
+                         search_t_sec=self.search_t_sec)
 
     def should_be_login_link(self):
         assert self.is_elem_present(*Mpl.LOGIN_LINK),\
